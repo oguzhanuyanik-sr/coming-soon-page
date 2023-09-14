1264cada739f27b3688940a91ded441d8f7e5f95
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-09-14 07:00:00 +0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -6,15 +6,26 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: { sm: '640px', md: '768px', lg: '1024px', xl: '1280px' },
+    colors: {
+      'white-100': '#FFFFFF',
+      'red-100': '#FF5466',
+      'blue-100': '#4C7BF3',
+      'grey-100': '#969696',
+      'grey-200': '#15202A',
+    },
+    fontFamily: {},
+    boxShadow: {},
+    backgroundImage: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: { sm: '640px', md: '768px', lg: '1024px', xl: '1280px' },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
